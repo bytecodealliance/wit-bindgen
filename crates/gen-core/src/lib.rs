fitzgen/witx-bindgen

Update to the latest abi-next branch.

crates/gen-core/src/lib.rs
@@ -1,11 +1,11 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, HashSet, BTreeMap, btree_map::Entry};
 use witx::*;
 
 pub use witx;
 
 pub trait Generator {
-    fn preprocess(&mut self, doc: &Document, import: bool) {
-        drop((doc, import));
+    fn preprocess(&mut self, m: &Module, import: bool) {
+        drop((m, import));
     }
 
     fn type_record(&mut self, name: &Id, record: &RecordDatatype, docs: &str);
@@ -17,47 +17,45 @@ pub trait Generator {
     fn type_builtin(&mut self, name: &Id, ty: BuiltinType, docs: &str);
     fn type_buffer(&mut self, name: &Id, ty: &Buffer, docs: &str);
     fn const_(&mut self, name: &Id, ty: &Id, val: u64, docs: &str);
-    fn import(&mut self, module: &Id, func: &InterfaceFunc);
-    fn export(&mut self, module: &Id, func: &InterfaceFunc);
-    fn finish(&mut self) -> Files;
+    fn import(&mut self, module: &Id, func: &Function);
+    fn export(&mut self, module: &Id, func: &Function);
+    fn finish(&mut self, files: &mut Files);
 
-    fn generate(&mut self, doc: &Document, import: bool) -> Files {
-        self.preprocess(doc, import);
-        for ty in doc.typenames() {
+    fn generate(&mut self, m: &Module, import: bool, files: &mut Files) {
+        self.preprocess(m, import);
+        for ty in m.typenames() {
             let t = match &ty.tref {
                 TypeRef::Name(nt) => {
-                    self.type_alias(&ty.name, nt, &ty.docs);
+                    self.type_alias(&ty.name, &nt, &ty.docs);
                     continue;
                 }
                 TypeRef::Value(t) => t,
             };
             match &**t {
-                Type::Record(t) => self.type_record(&ty.name, t, &ty.docs),
-                Type::Variant(t) => self.type_variant(&ty.name, t, &ty.docs),
-                Type::Handle(t) => self.type_handle(&ty.name, t, &ty.docs),
-                Type::List(t) => self.type_list(&ty.name, t, &ty.docs),
-                Type::Pointer(t) => self.type_pointer(&ty.name, false, t, &ty.docs),
-                Type::ConstPointer(t) => self.type_pointer(&ty.name, true, t, &ty.docs),
+                Type::Record(t) => self.type_record(&ty.name, &t, &ty.docs),
+                Type::Variant(t) => self.type_variant(&ty.name, &t, &ty.docs),
+                Type::Handle(t) => self.type_handle(&ty.name, &t, &ty.docs),
+                Type::List(t) => self.type_list(&ty.name, &t, &ty.docs),
+                Type::Pointer(t) => self.type_pointer(&ty.name, false, &t, &ty.docs),
+                Type::ConstPointer(t) => self.type_pointer(&ty.name, true, &t, &ty.docs),
                 Type::Builtin(t) => self.type_builtin(&ty.name, *t, &ty.docs),
-                Type::Buffer(b) => self.type_buffer(&ty.name, b, &ty.docs),
+                Type::Buffer(b) => self.type_buffer(&ty.name, &b, &ty.docs),
             }
         }
 
-        for c in doc.constants() {
+        for c in m.constants() {
             self.const_(&c.name, &c.ty, c.value, &c.docs);
         }
 
-        for m in doc.modules() {
-            for f in m.funcs() {
-                if import {
-                    self.import(&m.name, &f);
-                } else {
-                    self.export(&m.name, &f);
-                }
+        for f in m.funcs() {
+            if import {
+                self.import(&m.name(), &f);
+            } else {
+                self.export(&m.name(), &f);
             }
         }
 
-        self.finish()
+        self.finish(files)
     }
 }
 
@@ -105,25 +103,23 @@ impl std::ops::BitOrAssign for TypeInfo {
 }
 
 impl Types {
-    pub fn analyze(&mut self, doc: &Document) {
-        for t in doc.typenames() {
+    pub fn analyze(&mut self, m: &Module) {
+        for t in m.typenames() {
             let info = self.type_ref_info(&t.tref);
             self.type_info.insert(t.name.clone(), info);
         }
-        for m in doc.modules() {
-            for f in m.funcs() {
-                for param in f.params.iter() {
-                    self.set_param_result_tref(&param.tref, true, false);
-                }
-                for param in f.results.iter() {
-                    self.set_param_result_tref(&param.tref, false, true);
-                }
-                self.maybe_set_dtor(doc, &f);
+        for f in m.funcs() {
+            for param in f.params.iter() {
+                self.set_param_result_tref(&param.tref, true, false);
             }
+            for param in f.results.iter() {
+                self.set_param_result_tref(&param.tref, false, true);
+            }
+            self.maybe_set_dtor(m, &f);
         }
     }
 
-    fn maybe_set_dtor(&mut self, doc: &Document, f: &InterfaceFunc) {
+    fn maybe_set_dtor(&mut self, m: &Module, f: &Function) {
         // Dtors only happen when the function has a singular parameter
         if f.params.len() != 1 {
             return;
@@ -150,7 +146,7 @@ impl Types {
 
         // ... and finally the actual type of this value must be a `(handle)`
         let id = Id::new(prefix);
-        let ty = match doc.typename(&id) {
+        let ty = match m.typename(&id) {
             Some(ty) => ty,
             None => return,
         };
@@ -257,12 +253,19 @@ impl Types {
 
 #[derive(Default)]
 pub struct Files {
-    files: Vec<(String, String)>,
+    files: BTreeMap<String, String>,
 }
 
 impl Files {
     pub fn push(&mut self, name: &str, contents: &str) {
-        self.files.push((name.to_string(), contents.to_string()));
+        match self.files.entry(name.to_owned()) {
+            Entry::Vacant(entry) => {
+                entry.insert(contents.to_owned());
+            }
+            Entry::Occupied(ref mut entry) => {
+                entry.get_mut().push_str(contents);
+            }
+        }
     }
 
     pub fn iter(&self) -> impl Iterator<Item = (&'_ str, &'_ str)> {

crates/gen-rust-wasm/src/lib.rs
@@ -159,16 +159,12 @@ impl TypePrint for RustWasm {
 }
 
 impl Generator for RustWasm {
-    fn preprocess(&mut self, doc: &Document, import: bool) {
+    fn preprocess(&mut self, m: &Module, import: bool) {
         self.in_import = import;
-        self.types.analyze(doc);
-        assert!(
-            doc.modules().count() <= 1,
-            "only one module supported at this time"
-        );
-        if let Some(m) = doc.modules().next() {
-            self.trait_name = m.name.as_str().to_camel_case();
-        }
+        self.types.analyze(m);
+        self.trait_name = m.name().as_str().to_camel_case();
+        self.src
+            .push_str(&format!("mod {} {{", m.name().as_str().to_snake_case()));
     }
 
     fn type_record(&mut self, name: &Id, record: &RecordDatatype, docs: &str) {
@@ -295,7 +291,7 @@ impl Generator for RustWasm {
         ));
     }
 
-    fn import(&mut self, module: &Id, func: &InterfaceFunc) {
+    fn import(&mut self, module: &Id, func: &Function) {
         self.is_dtor = self.types.is_dtor_func(&func.name);
         self.params = self.print_signature(
             func,
@@ -328,7 +324,7 @@ impl Generator for RustWasm {
         self.src.push_str("}");
     }
 
-    fn export(&mut self, module: &Id, func: &InterfaceFunc) {
+    fn export(&mut self, module: &Id, func: &Function) {
         self.is_dtor = self.types.is_dtor_func(&func.name);
         let rust_name = func.name.as_ref().to_snake_case();
 
@@ -386,9 +382,7 @@ impl Generator for RustWasm {
         trait_.handles.extend(mem::take(&mut self.handles_for_func));
     }
 
-    fn finish(&mut self) -> Files {
-        let mut files = Files::default();
-
+    fn finish(&mut self, files: &mut Files) {
         let mut src = mem::take(&mut self.src);
 
         for (name, trait_) in self.traits.iter() {
@@ -429,8 +423,11 @@ impl Generator for RustWasm {
             let status = child.wait().unwrap();
             assert!(status.success());
         }
+
+        // Close the opening `mod`.
+        src.push_str("}");
+
         files.push("bindings.rs", &src);
-        files
     }
 }
 

crates/gen-wasmtime/src/lib.rs
@@ -320,16 +320,12 @@ impl TypePrint for Wasmtime {
 }
 
 impl Generator for Wasmtime {
-    fn preprocess(&mut self, doc: &Document, import: bool) {
-        assert!(
-            doc.modules().count() <= 1,
-            "only one module supported at this time"
-        );
-        self.types.analyze(doc);
+    fn preprocess(&mut self, m: &Module, import: bool) {
+        self.types.analyze(m);
         self.in_import = import;
-        if let Some(m) = doc.modules().next() {
-            self.trait_name = m.name.as_str().to_camel_case();
-        }
+        self.trait_name = m.name().as_str().to_camel_case();
+        self.src
+            .push_str(&format!("mod {} {{", m.name().as_str().to_snake_case()));
     }
 
     fn type_record(&mut self, name: &Id, record: &RecordDatatype, docs: &str) {
@@ -490,7 +486,7 @@ impl Generator for Wasmtime {
         ));
     }
 
-    fn import(&mut self, module: &Id, func: &InterfaceFunc) {
+    fn import(&mut self, module: &Id, func: &Function) {
         self.tmp = 0;
         let prev = mem::take(&mut self.src);
         self.is_dtor = self.types.is_dtor_func(&func.name);
@@ -589,7 +585,7 @@ impl Generator for Wasmtime {
         assert!(self.cleanup.is_none());
     }
 
-    fn export(&mut self, module: &Id, func: &InterfaceFunc) {
+    fn export(&mut self, module: &Id, func: &Function) {
         self.tmp = 0;
         let prev = mem::take(&mut self.src);
         self.is_dtor = self.types.is_dtor_func(&func.name);
@@ -674,9 +670,7 @@ impl Generator for Wasmtime {
         );
     }
 
-    fn finish(&mut self) -> Files {
-        let mut files = Files::default();
-
+    fn finish(&mut self, files: &mut Files) {
         for (module, funcs) in sorted_iter(&self.imports) {
             self.src.push_str("\npub trait ");
             self.src.push_str(&module.as_str().to_camel_case());
@@ -901,8 +895,11 @@ impl Generator for Wasmtime {
             let status = child.wait().unwrap();
             assert!(status.success());
         }
+
+        // Close the opening `mod`.
+        src.push_str("}");
+
         files.push("bindings.rs", &src);
-        files
     }
 }
 

src/bin/witx-bindgen.rs
@@ -1,7 +1,7 @@
 use anyhow::{Context, Result};
 use std::path::PathBuf;
 use structopt::StructOpt;
-use witx_bindgen_gen_core::{witx, Generator};
+use witx_bindgen_gen_core::{witx, Files, Generator};
 
 #[derive(Debug, StructOpt)]
 struct Opt {
@@ -54,8 +54,12 @@ fn main() -> Result<()> {
         anyhow::bail!("one of `--import` or `--export` must be used");
     }
 
-    let doc = witx::load(&common.witx)?;
-    let files = generator.generate(&doc, common.import);
+    let mut files = Files::default();
+    for witx in common.witx {
+        let m = witx::load(witx)?;
+        generator.generate(&m, common.import, &mut files);
+    }
+
     for (name, contents) in files.iter() {
         let dst = match &common.out_dir {
             Some(path) => path.join(name),

bytecodealliance/wit-bindgen

crates/gen-core/src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, HashSet, BTreeMap, btree_map::Entry};
 use witx::*;
 
 pub use witx;
@@ -19,9 +19,9 @@ pub trait Generator {
     fn const_(&mut self, name: &Id, ty: &Id, val: u64, docs: &str);
     fn import(&mut self, module: &Id, func: &Function);
     fn export(&mut self, module: &Id, func: &Function);
-    fn finish(&mut self) -> Files;
+    fn finish(&mut self, files: &mut Files);
 
-    fn generate(&mut self, module: &Module, import: bool) -> Files {
+    fn generate(&mut self, module: &Module, import: bool, files: &mut Files) {
         self.preprocess(module, import);
         for ty in module.typenames() {
             let t = match &ty.tref {
@@ -49,13 +49,13 @@ pub trait Generator {
 
         for f in module.funcs() {
             if import {
-                self.import(module.name(), &f);
+                self.import(&module.name(), &f);
             } else {
-                self.export(module.name(), &f);
+                self.export(&module.name(), &f);
             }
         }
 
-        self.finish()
+        self.finish(files)
     }
 }
 
@@ -253,12 +253,19 @@ impl Types {
 
 #[derive(Default)]
 pub struct Files {
-    files: Vec<(String, String)>,
+    files: BTreeMap<String, String>,
 }
 
 impl Files {
     pub fn push(&mut self, name: &str, contents: &str) {
-        self.files.push((name.to_string(), contents.to_string()));
+        match self.files.entry(name.to_owned()) {
+            Entry::Vacant(entry) => {
+                entry.insert(contents.to_owned());
+            }
+            Entry::Occupied(ref mut entry) => {
+                entry.get_mut().push_str(contents);
+            }
+        }
     }
 
     pub fn iter(&self) -> impl Iterator<Item = (&'_ str, &'_ str)> {

crates/gen-rust-wasm/src/lib.rs
@@ -163,6 +163,8 @@ impl Generator for RustWasm {
         self.in_import = import;
         self.types.analyze(module);
         self.trait_name = module.name().as_str().to_camel_case();
+        self.src
+            .push_str(&format!("mod {} {{", module.name().as_str().to_snake_case()));
     }
 
     fn type_record(&mut self, name: &Id, record: &RecordDatatype, docs: &str) {
@@ -380,9 +382,7 @@ impl Generator for RustWasm {
         trait_.handles.extend(mem::take(&mut self.handles_for_func));
     }
 
-    fn finish(&mut self) -> Files {
-        let mut files = Files::default();
-
+    fn finish(&mut self, files: &mut Files) {
         let mut src = mem::take(&mut self.src);
 
         for (name, trait_) in self.traits.iter() {
@@ -423,8 +423,11 @@ impl Generator for RustWasm {
             let status = child.wait().unwrap();
             assert!(status.success());
         }
+
+        // Close the opening `mod`.
+        src.push_str("}");
+
         files.push("bindings.rs", &src);
-        files
     }
 }
 

crates/gen-wasmtime/src/lib.rs
@@ -324,6 +324,8 @@ impl Generator for Wasmtime {
         self.types.analyze(module);
         self.in_import = import;
         self.trait_name = module.name().as_str().to_camel_case();
+        self.src
+            .push_str(&format!("mod {} {{", module.name().as_str().to_snake_case()));
     }
 
     fn type_record(&mut self, name: &Id, record: &RecordDatatype, docs: &str) {
@@ -668,9 +670,7 @@ impl Generator for Wasmtime {
         );
     }
 
-    fn finish(&mut self) -> Files {
-        let mut files = Files::default();
-
+    fn finish(&mut self, files: &mut Files) {
         for (module, funcs) in sorted_iter(&self.imports) {
             self.src.push_str("\npub trait ");
             self.src.push_str(&module.as_str().to_camel_case());
@@ -895,8 +895,11 @@ impl Generator for Wasmtime {
             let status = child.wait().unwrap();
             assert!(status.success());
         }
+
+        // Close the opening `mod`.
+        src.push_str("}");
+
         files.push("bindings.rs", &src);
-        files
     }
 }
 

src/bin/witx-bindgen.rs
@@ -1,7 +1,7 @@
 use anyhow::{Context, Result};
 use std::path::PathBuf;
 use structopt::StructOpt;
-use witx_bindgen_gen_core::{witx, Generator};
+use witx_bindgen_gen_core::{witx, Files, Generator};
 
 #[derive(Debug, StructOpt)]
 struct Opt {
@@ -54,16 +54,12 @@ fn main() -> Result<()> {
         anyhow::bail!("one of `--import` or `--export` must be used");
     }
 
-    anyhow::ensure!(
-        common.witx.len() < 2,
-        "at most one *.witx file is supported right now"
-    );
-    if common.witx.is_empty() {
-        return Ok(());
+    let mut files = Files::default();
+    for witx in common.witx {
+        let module = witx::load(witx)?;
+        generator.generate(&module, common.import, &mut files);
     }
 
-    let doc = witx::load(&common.witx[0])?;
-    let files = generator.generate(&doc, common.import);
     for (name, contents) in files.iter() {
         let dst = match &common.out_dir {
             Some(path) => path.join(name),