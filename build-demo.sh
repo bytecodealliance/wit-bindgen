b76227f346f4ecfe8a04237aa13fec834541441d
Alex Crichton <alex@alexcrichton.com>
2021-06-17 16:48:27 -0700

    add build script

---
+++ build-demo.sh
@@ -0,0 +1,22 @@
+set -ex
+
+rm -rf static
+mkdir static
+
+cargo build -p demo --target wasm32-unknown-unknown --release
+cp target/wasm32-unknown-unknown/release/demo.wasm static/
+
+cargo run -- js --import crates/demo/browser.witx --no-typescript --out-dir static/browser
+cargo run -- js --export crates/demo/demo.witx --no-typescript --out-dir static/demo
+
+cp crates/demo/index.html static/
+cp crates/demo/main.js static/
+
+if [ ! -d ace ]; then
+  mkdir ace
+  cd ace
+  curl -L https://github.com/ajaxorg/ace-builds/archive/refs/tags/v1.4.12.tar.gz | tar xzf -
+  cd ..
+fi
+
+cp -r ace/ace-builds-1.4.12/src static/ace
