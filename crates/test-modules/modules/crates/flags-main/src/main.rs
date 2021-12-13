baa6711138a9f3184124bc1dae6cc77cb4dc219b
Dan Gohman <dev@sunfishcode.online>
2021-12-13 12:35:37 -0800

    rustfmt

--- crates/test-modules/modules/crates/flags-main/src/main.rs
+++ crates/test-modules/modules/crates/flags-main/src/main.rs
@@ -8,11 +8,17 @@ fn main() {
 
     assert_eq!(roundtrip_flag2(Flag2::empty()), Flag2::empty());
     assert_eq!(roundtrip_flag2(Flag2::B0), Flag2::B0);
-    assert_eq!(roundtrip_flag2(Flag2::B1 | Flag2::B0), Flag2::B1 | Flag2::B0);
+    assert_eq!(
+        roundtrip_flag2(Flag2::B1 | Flag2::B0),
+        Flag2::B1 | Flag2::B0
+    );
 
     assert_eq!(roundtrip_flag4(Flag4::empty()), Flag4::empty());
     assert_eq!(roundtrip_flag4(Flag4::B0), Flag4::B0);
-    assert_eq!(roundtrip_flag4(Flag4::B1 | Flag4::B0), Flag4::B1 | Flag4::B0);
+    assert_eq!(
+        roundtrip_flag4(Flag4::B1 | Flag4::B0),
+        Flag4::B1 | Flag4::B0
+    );
     assert_eq!(
         roundtrip_flag4(Flag4::B2 | Flag4::B1 | Flag4::B0),
         Flag4::B2 | Flag4::B1 | Flag4::B0
