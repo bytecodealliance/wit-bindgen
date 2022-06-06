d9798134b1488b2ca2b9ca2b05ea37bc89a67c58
Kyle Brown <kylebrw@gmail.com>
2022-06-06 10:28:26 -0400

    Create development container for wit-bindgen (#229)
    * Create dev container for testing wit-bindgen
    
    * Rename container, update to WASI SDK 16.0
    
    Co-authored-by: Kyle Brown <kyleb@liquidrocketry.com>

---
+++ .devcontainer/finalize.sh
@@ -0,0 +1,16 @@
+printf "Running 'postCreateCommand' Script\n"
+
+# Install Rust Targets
+printf "Installing Rust Targets\n"
+rustup update stable --no-self-update
+rustup default stable
+rustup target add wasm32-unknown-unknown
+rustup target add wasm32-wasi
+
+# Install Python stuff
+printf "Installing Python Dependencies"
+pip install mypy wasmtime
+
+# Install NPM dependencies
+printf "Installing NPM Dependencies"
+cd crates/gen-js && npm install
