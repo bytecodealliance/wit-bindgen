465551506859012bfe79a90401345031cdf12aad
Pat Hickey <phickey@fastly.com>
2022-09-29 12:18:50 -0700

    smuggle wit information in custom sections

--- crates/wit-component/tests/components.rs
+++ crates/wit-component/tests/components.rs
@@ -1,7 +1,7 @@
 use anyhow::{bail, Context, Result};
 use pretty_assertions::assert_eq;
 use std::{fs, path::Path};
-use wit_component::ComponentEncoder;
+use wit_component::{decode_interface_component, ComponentEncoder, InterfaceEncoder};
 use wit_parser::Interface;
 
 fn read_interface(path: &Path) -> Result<Interface> {
@@ -78,7 +78,7 @@ fn read_adapters(dir: &Path) -> Result<Vec<(String, Vec<u8>, Interface)>> {
 /// Run the test with the environment variable `BLESS` set to update
 /// either `component.wat` or `error.txt` depending on the outcome of the encoding.
 #[test]
-fn component_encoding() -> Result<()> {
+fn component_encoding_via_flags() -> Result<()> {
     drop(env_logger::try_init());
 
     for entry in fs::read_dir("tests/components")? {
@@ -116,6 +116,160 @@ fn component_encoding() -> Result<()> {
 
         if let Some(interface) = &interface {
             encoder = encoder.interface(interface);
+        }
+
+        let r = encoder.encode();
+        let (output, baseline_path) = if error_path.is_file() {
+            match r {
+                Ok(_) => bail!("encoding should fail for test case `{}`", test_case),
+                Err(e) => (e.to_string(), &error_path),
+            }
+        } else {
+            (
+                wasmprinter::print_bytes(
+                    &r.with_context(|| format!("failed to encode for test case `{}`", test_case))?,
+                )
+                .with_context(|| {
+                    format!(
+                        "failed to print component bytes for test case `{}`",
+                        test_case
+                    )
+                })?,
+                &component_path,
+            )
+        };
+
+        if std::env::var_os("BLESS").is_some() {
+            fs::write(&baseline_path, output)?;
+        } else {
+            assert_eq!(
+                fs::read_to_string(&baseline_path)?.replace("\r\n", "\n"),
+                output,
+                "failed baseline comparison for test case `{}` ({})",
+                test_case,
+                baseline_path.display(),
+            );
+        }
+    }
+
+    Ok(())
+}
+
+/// Tests the encoding of components.
+///
+/// This test looks in the `components/` directory for test cases.
+///
+/// The expected input files for a test case are:
+///
+/// * [required] `module.wat` - contains the core module definition to be encoded
+///   as a component.
+/// * [optional] `default.wit` - represents the component's default interface.
+/// * [optional] `export-<name>.wit` - represents an interface exported by the component.
+/// * [optional] `import-<name>.wit` - represents an interface imported by the component.
+///
+/// And the output files are one of the following:
+///
+/// * `component.wat` - the expected encoded component in text format if the encoding
+///   is expected to succeed.
+/// * `error.txt` - the expected error message if the encoding is expected to fail.
+///
+/// The test encodes a component based on the input files. If the encoding succeeds,
+/// it expects the output to match `component.wat`. If the encoding fails, it expects
+/// the output to match `error.txt`.
+///
+/// Run the test with the environment variable `BLESS` set to update
+/// either `component.wat` or `error.txt` depending on the outcome of the encoding.
+#[test]
+fn component_encoding_via_custom_section() -> Result<()> {
+    use wasm_encoder::{Encode, Section};
+
+    for entry in fs::read_dir("tests/components")? {
+        let path = entry?.path();
+        if !path.is_dir() {
+            continue;
+        }
+
+        let test_case = path.file_stem().unwrap().to_str().unwrap();
+
+        let module_path = path.join("module.wat");
+        let interface_path = path.join("default.wit");
+        let component_path = path.join("component.wat");
+        let error_path = path.join("error.txt");
+
+        let mut module = wat::parse_file(&module_path)
+            .with_context(|| format!("expected file `{}`", module_path.display()))?;
+
+        let base_module = module.clone();
+
+        fn encode_interface(i: &Interface, module: &mut Vec<u8>, kind: &str) -> Result<()> {
+            let name = &format!("component-type:{}:{}", kind, i.name);
+            let contents = InterfaceEncoder::new(&i).validate(true).encode()?;
+            let section = wasm_encoder::CustomSection {
+                name,
+                data: &contents,
+            };
+            module.push(section.id());
+            section.encode(module);
+            Ok(())
+        }
+
+        if interface_path.is_file() {
+            let i = read_interface(&interface_path)?;
+            encode_interface(&i, &mut module, "export")?;
+        }
+        for i in read_interfaces(&path, "import-*.wit")? {
+            encode_interface(&i, &mut module, "import")?;
+        }
+        for i in read_interfaces(&path, "export-*.wit")? {
+            encode_interface(&i, &mut module, "export-instance")?;
+        }
+
+        // Everything is now encoded into `module`: now lets deconstruct it
+
+        let mut encoder = ComponentEncoder::default()
+            .module(&base_module)
+            .validate(true);
+
+        let mut interface = None;
+        let mut imports = Vec::new();
+        let mut exports = Vec::new();
+        for payload in wasmparser::Parser::new(0).parse_all(&module) {
+            match payload.context("decoding item in module")? {
+                wasmparser::Payload::CustomSection(cs) => {
+                    if let Some(export) = cs.name().strip_prefix("component-type:export:") {
+                        let mut i = decode_interface_component(cs.data()).with_context(|| {
+                            format!("decoding component-type in export section {}", export)
+                        })?;
+                        i.name = export.to_owned();
+                        interface = Some(i);
+                    } else if let Some(import) = cs.name().strip_prefix("component-type:import:") {
+                        let mut interface =
+                            decode_interface_component(cs.data()).with_context(|| {
+                                format!("decoding component-type in import section {}", import)
+                            })?;
+                        interface.name = import.to_owned();
+                        imports.push(interface);
+                    } else if let Some(export_instance) =
+                        cs.name().strip_prefix("component-type:export-instance:")
+                    {
+                        let mut interface =
+                            decode_interface_component(cs.data()).with_context(|| {
+                                format!(
+                                    "decoding component-type in export-instance section {}",
+                                    export_instance
+                                )
+                            })?;
+                        interface.name = export_instance.to_owned();
+                        exports.push(interface);
+                    }
+                }
+                _ => {}
+            }
+        }
+
+        encoder = encoder.imports(&imports).exports(&exports);
+        if let Some(interface) = &interface {
+            encoder = encoder.interface(&interface);
         }
 
         let r = encoder.encode();
