0caee95c3d77c27f6e7ad8109d7087f22c955fe5
Alex Crichton <alex@alexcrichton.com>
2022-10-05 08:48:33 -0700

    Compile test cases with `wasm32-wasi` again
    This commit is the next step in integrating `wasm32-wasi`,
    `wit-bindgen`, tests, and components all together. Tests are now again
    compiled with `wasm32-wasi` and use a repo-specific adapter module
    (with support from #338) to support transforming the final module into
    an actual component.
    
    In supporting this feature the support from #331 is refactored into a
    new `extract` Rust module so the functionality can be shared between
    the ingestion of the main module as well as ingestion of adapter
    modules. Adapter modules now are also supported on the CLI as a
    standalone file without having to specify other options.
    
    Note that the actual `wasi_snapshot_preview1.wasm` adapter is
    non-functional in this commit and doesn't do anything fancy. The tests
    in this repository don't really need all that much and I suspect all
    we'll really need to implement is `fd_write` for fd 1 (as that's
    stdout).

---
+++ crates/wasi_snapshot_preview1/src/lib.rs
@@ -0,0 +1,44 @@
+//! This module is intended to be an "adapter module" fed into `wit-component`
+//! to translate the `wasi_snapshot_preview1` ABI into an ABI that uses the
+//! component model. This library is compiled as a standalone wasm file and is
+//! used to implement `wasi_snapshot_preview1` interfaces required by the tests
+//! throughout the `wit-bindgen` repository.
+//!
+//! This is not intended to be a comprehensive polyfill. Instead this is just
+//! the bare bones necessary to get `wit-bindgen` itself and its tests working.
+//!
+//! Currently all functions are trapping stubs since nothing actually runs the
+//! output component just yet. These stubs should get filled in as necessary
+//! once hosts start running components. The current assumption is that the
+//! imports will be adapted to a custom `wit-bindgen`-specific host `*.wit` file
+//! which is only suitable for `wit-bindgen` tests.
+
+#![allow(unused_variables)]
+
+use std::arch::wasm32::unreachable;
+use wasi::*;
+
+#[no_mangle]
+pub extern "C" fn environ_get(environ: *mut *mut u8, environ_buf: *mut u8) -> Errno {
+    unreachable()
+}
+
+#[no_mangle]
+pub extern "C" fn environ_sizes_get(environc: *mut Size, environ_buf_size: *mut Size) -> Errno {
+    unreachable()
+}
+
+#[no_mangle]
+pub extern "C" fn fd_write(
+    fd: Fd,
+    iovs_ptr: *const Ciovec,
+    iovs_len: usize,
+    nwritten: *mut Size,
+) -> Errno {
+    unreachable()
+}
+
+#[no_mangle]
+pub extern "C" fn proc_exit(rval: Exitcode) -> ! {
+    unreachable()
+}
