31b737c7cce9ae31248666f9a6afd81999ccbc1e
Pat Hickey <phickey@fastly.com>
2022-10-05 14:55:49 -0700

    test-helpers/build.rs: test encoding teavm guests as a component as well

--- crates/test-helpers/build.rs
+++ crates/test-helpers/build.rs
@@ -251,15 +251,35 @@ fn main() {
                 panic!("failed to build");
             }
 
+            let out_wasm = out_dir.join("target/generated/wasm/teavm-wasm/classes.wasm");
+
             wasms.push((
                 "java",
                 test_dir.file_stem().unwrap().to_str().unwrap().to_string(),
-                out_dir
-                    .join("target/generated/wasm/teavm-wasm/classes.wasm")
-                    .to_str()
-                    .unwrap()
-                    .to_string(),
+                out_wasm.to_str().unwrap().to_string(),
             ));
+
+            let imports = [Interface::parse_file(test_dir.join("imports.wit")).unwrap()];
+            let interface = Interface::parse_file(test_dir.join("exports.wit")).unwrap();
+
+            // Validate that the module can be translated to a component, using
+            // wit interfaces explicitly passed to ComponentEncoder, because the
+            // TeaVM guest doesnt yet support putting component types into custom
+            // sections.
+            let module = fs::read(&out_wasm).expect("failed to read wasm file");
+            ComponentEncoder::default()
+                .imports(&imports)
+                .interface(&interface)
+                .module(module.as_slice())
+                .expect("pull custom sections from module")
+                .validate(true)
+                .adapter_file(&wasi_adapter)
+                .expect("adapter failed to get loaded")
+                .encode()
+                .expect(&format!(
+                    "module {:?} can be translated to a component",
+                    out_wasm
+                ));
         }
     }
 
