e0adcf295a7321643b6604329eec46d64deb5abd
Pat Hickey <phickey@fastly.com>
2022-10-10 11:34:22 -0700

    lists runtime test: imports get called with owned values

--- tests/runtime/lists/host.rs
+++ tests/runtime/lists/host.rs
@@ -3,17 +3,16 @@ use anyhow::Result;
 wit_bindgen_host_wasmtime_rust::export!("../../tests/runtime/lists/imports.wit");
 
 use imports::*;
-use wit_bindgen_host_wasmtime_rust::Le;
 
 #[derive(Default)]
 pub struct MyImports;
 
 impl Imports for MyImports {
-    fn empty_list_param(&mut self, a: &[u8]) {
+    fn empty_list_param(&mut self, a: Vec<u8>) {
         assert_eq!(a, []);
     }
 
-    fn empty_string_param(&mut self, a: &str) {
+    fn empty_string_param(&mut self, a: String) {
         assert_eq!(a, "");
     }
 
@@ -25,22 +24,22 @@ impl Imports for MyImports {
         String::new()
     }
 
-    fn list_param(&mut self, list: &[u8]) {
+    fn list_param(&mut self, list: Vec<u8>) {
         assert_eq!(list, [1, 2, 3, 4]);
     }
 
-    fn list_param2(&mut self, ptr: &str) {
+    fn list_param2(&mut self, ptr: String) {
         assert_eq!(ptr, "foo");
     }
 
-    fn list_param3(&mut self, ptr: Vec<&str>) {
+    fn list_param3(&mut self, ptr: Vec<String>) {
         assert_eq!(ptr.len(), 3);
         assert_eq!(ptr[0], "foo");
         assert_eq!(ptr[1], "bar");
         assert_eq!(ptr[2], "baz");
     }
 
-    fn list_param4(&mut self, ptr: Vec<Vec<&str>>) {
+    fn list_param4(&mut self, ptr: Vec<Vec<String>>) {
         assert_eq!(ptr.len(), 2);
         assert_eq!(ptr[0][0], "foo");
         assert_eq!(ptr[0][1], "bar");
@@ -59,61 +58,49 @@ impl Imports for MyImports {
         vec!["hello,".to_string(), "world!".to_string()]
     }
 
-    fn list_roundtrip(&mut self, list: &[u8]) -> Vec<u8> {
+    fn list_roundtrip(&mut self, list: Vec<u8>) -> Vec<u8> {
         list.to_vec()
     }
 
-    fn string_roundtrip(&mut self, s: &str) -> String {
+    fn string_roundtrip(&mut self, s: String) -> String {
         s.to_string()
     }
 
-    fn list_minmax8(&mut self, u: &[u8], s: &[i8]) -> (Vec<u8>, Vec<i8>) {
+    fn list_minmax8(&mut self, u: Vec<u8>, s: Vec<i8>) -> (Vec<u8>, Vec<i8>) {
         assert_eq!(u, [u8::MIN, u8::MAX]);
         assert_eq!(s, [i8::MIN, i8::MAX]);
-        (u.to_vec(), s.to_vec())
+        (u, s)
     }
 
-    fn list_minmax16(&mut self, u: &[Le<u16>], s: &[Le<i16>]) -> (Vec<u16>, Vec<i16>) {
+    fn list_minmax16(&mut self, u: Vec<u16>, s: Vec<i16>) -> (Vec<u16>, Vec<i16>) {
         assert_eq!(u, [u16::MIN, u16::MAX]);
         assert_eq!(s, [i16::MIN, i16::MAX]);
-        (
-            u.iter().map(|e| e.get()).collect(),
-            s.iter().map(|e| e.get()).collect(),
-        )
+        (u, s)
     }
 
-    fn list_minmax32(&mut self, u: &[Le<u32>], s: &[Le<i32>]) -> (Vec<u32>, Vec<i32>) {
+    fn list_minmax32(&mut self, u: Vec<u32>, s: Vec<i32>) -> (Vec<u32>, Vec<i32>) {
         assert_eq!(u, [u32::MIN, u32::MAX]);
         assert_eq!(s, [i32::MIN, i32::MAX]);
-        (
-            u.iter().map(|e| e.get()).collect(),
-            s.iter().map(|e| e.get()).collect(),
-        )
+        (u, s)
     }
 
-    fn list_minmax64(&mut self, u: &[Le<u64>], s: &[Le<i64>]) -> (Vec<u64>, Vec<i64>) {
+    fn list_minmax64(&mut self, u: Vec<u64>, s: Vec<i64>) -> (Vec<u64>, Vec<i64>) {
         assert_eq!(u, [u64::MIN, u64::MAX]);
         assert_eq!(s, [i64::MIN, i64::MAX]);
-        (
-            u.iter().map(|e| e.get()).collect(),
-            s.iter().map(|e| e.get()).collect(),
-        )
+        (u, s)
     }
 
-    fn list_minmax_float(&mut self, u: &[Le<f32>], s: &[Le<f64>]) -> (Vec<f32>, Vec<f64>) {
+    fn list_minmax_float(&mut self, u: Vec<f32>, s: Vec<f64>) -> (Vec<f32>, Vec<f64>) {
         assert_eq!(u, [f32::MIN, f32::MAX, f32::NEG_INFINITY, f32::INFINITY]);
         assert_eq!(s, [f64::MIN, f64::MAX, f64::NEG_INFINITY, f64::INFINITY]);
-        (
-            u.iter().map(|e| e.get()).collect(),
-            s.iter().map(|e| e.get()).collect(),
-        )
+        (u, s)
     }
 
     fn unaligned_roundtrip1(
         &mut self,
-        u16s: &[Le<u16>],
-        u32s: &[Le<u32>],
-        u64s: &[Le<u64>],
+        u16s: Vec<u16>,
+        u32s: Vec<u32>,
+        u64s: Vec<u64>,
         flag32s: Vec<Flag32>,
         flag64s: Vec<Flag64>,
     ) {
@@ -126,15 +113,15 @@ impl Imports for MyImports {
 
     fn unaligned_roundtrip2(
         &mut self,
-        records: &[Le<UnalignedRecord>],
-        f32s: &[Le<f32>],
-        f64s: &[Le<f64>],
-        strings: Vec<&str>,
-        lists: Vec<&[u8]>,
+        records: Vec<UnalignedRecord>,
+        f32s: Vec<f32>,
+        f64s: Vec<f64>,
+        strings: Vec<String>,
+        lists: Vec<Vec<u8>>,
     ) {
         assert_eq!(records.len(), 1);
-        assert_eq!(records[0].get().a, 10);
-        assert_eq!(records[0].get().b, 11);
+        assert_eq!(records[0].a, 10);
+        assert_eq!(records[0].b, 11);
         assert_eq!(f32s, [100.0]);
         assert_eq!(f64s, [101.0]);
         assert_eq!(strings, ["foo"]);
@@ -150,7 +137,7 @@ fn run(wasm: &str) -> Result<()> {
     let (exports, mut store) = crate::instantiate(
         wasm,
         |linker| imports::add_to_linker(linker, |cx| -> &mut MyImports { &mut cx.imports }),
-        |store, module, linker| Exports::instantiate(store, module, linker, |cx| &mut cx.exports),
+        |store, module, linker| Exports::instantiate(store, module, linker),
     )?;
 
     let bytes = exports.allocated_bytes(&mut store)?;
