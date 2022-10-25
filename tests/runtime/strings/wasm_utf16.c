9dca1e227d1f18cabb93835ff94f15f5eda288ac
Guy Bedford <gbedford@fastly.com>
2022-10-25 12:03:16 -0700

    actually fix c string assertion

--- tests/runtime/strings/wasm_utf16.c
+++ tests/runtime/strings/wasm_utf16.c
@@ -14,19 +14,19 @@ char16_t STR_BUFFER[500];
 
 void assert_str(imports_string_t* str, char16_t* expected, size_t expected_len) {
   assert(str->len == expected_len);
-  assert(memcmp(str->ptr, expected, 11) == 0);
+  assert(memcmp(str->ptr, expected, expected_len * 2) == 0);
 }
 
 void exports_test_imports() {
   imports_string_t str1;
   imports_string_set(&str1, BASIC_STRING);
-  assert_str(&str1, &BASIC_STRING[0], str1.len);
+  assert_str(&str1, &BASIC_STRING[0], 11);
   imports_f1(&str1);
   imports_string_t str2;
   imports_f2(&str2);
   memcpy(&STR_BUFFER[0], str2.ptr, str2.len * 2);
   STR_BUFFER[str2.len] = '\0';
-  assert_str(&str2, &UNICODE_STRING[0], str2.len);
+  assert_str(&str2, &UNICODE_STRING[0], 11);
 }
 
 void exports_f1(exports_string_t *str) {
