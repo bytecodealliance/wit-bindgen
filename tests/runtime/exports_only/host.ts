618964fcaf8f52a0f5e48ef34a5dd059a09d166c
Guy Bedford <gbedford@fastly.com>
2022-11-08 12:23:36 -0800

    update bound

--- tests/runtime/exports_only/host.ts
+++ tests/runtime/exports_only/host.ts
@@ -13,6 +13,6 @@ strictEqual(result, 'test');
 // Verify the inlined file size does not regress
 const url = new URL('./exports_only.js', import.meta.url);
 const jsSource = await readFile(url);
-const max_size = 1105;
+const max_size = 1200;
 ok(jsSource.byteLength <= max_size, `JS inlined bytelength ${jsSource.byteLength} is greater than ${max_size} bytes, at ${fileURLToPath(url)}`);
 
