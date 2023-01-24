0ef192e6d12da4c71de3218f43e99b81a3af7691
Alex Crichton <alex@alexcrichton.com>
2023-01-24 09:07:24 -0800

    Fix bindings generation for bare function exports
    Move around a `use` statement

--- crates/gen-guest-rust/src/lib.rs
+++ crates/gen-guest-rust/src/lib.rs
@@ -327,6 +327,9 @@ impl InterfaceGenerator<'_> {
             uwrite!(
                 self.src,
                 "
+                    #[allow(unused_imports)]
+                    use wit_bindgen_guest_rust::rt::{{alloc, vec::Vec, string::String}};
+
                     #[repr(align({align}))]
                     struct _RetArea([u8; {size}]);
                     static mut _RET_AREA: _RetArea = _RetArea([0; {size}]);
@@ -347,9 +350,6 @@ impl InterfaceGenerator<'_> {
             "
                 #[allow(clippy::all)]
                 pub mod {snake} {{
-                    #[allow(unused_imports)]
-                    use wit_bindgen_guest_rust::rt::{{alloc, vec::Vec, string::String}};
-
                     {module}
                 }}
             "
