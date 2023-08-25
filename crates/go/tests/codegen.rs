8312ab4ad048c0e970c78671e741f1c4a9b6516f
Alex Crichton <alex@alexcrichton.com>
2023-08-25 13:02:27 -0700

    Ignore a new test in the Go generator

--- crates/go/tests/codegen.rs
+++ crates/go/tests/codegen.rs
@@ -17,6 +17,7 @@ macro_rules! codegen_test {
     (resource_local_alias_borrow_import $name:tt $test:tt) => {};
     (resource_borrow_in_record $name:tt $test:tt) => {};
     (resource_borrow_in_record_export $name:tt $test:tt) => {};
+    (resources_in_aggregates $name:tt $test:tt) => {};
 
     ($id:ident $name:tt $test:tt) => {
         #[test]
