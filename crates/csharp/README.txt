f29eeb04ff328ec254d570ecc40d7589768eff91
yowl00 <scott.waye@hubse.com>
2023-10-24 10:40:04 -0500

    add csharp feature and start codegen tests.

---
+++ crates/csharp/README.txt
@@ -0,0 +1,3 @@
+// generate the c# and the component meta module
+
+cargo run c-sharp  --string-encoding utf8 --out-dir testing-csharp tests/codegen/floats.wit
---
+++ crates/csharp/src/component_type_object.rs
@@ -0,0 +1,58 @@
+use anyhow::Result;
+use heck::ToSnakeCase;
+use wasm_encoder::{
+    CodeSection, CustomSection, Function, FunctionSection, LinkingSection, Module, SymbolTable,
+    TypeSection,
+};
+use wit_bindgen_core::wit_parser::{Resolve, WorldId};
+use wit_component::StringEncoding;
+
+pub fn linking_symbol(name: &str) -> String {
+    let snake = name.to_snake_case();
+    format!("__component_type_object_force_link_{snake}")
+}
+
+pub fn object(resolve: &Resolve, world: WorldId, encoding: StringEncoding) -> Result<Vec<u8>> {
+    let mut module = Module::new();
+
+    // Build a module with one function that's a "dummy function"
+    let mut types = TypeSection::new();
+    types.function([], []);
+    module.section(&types);
+    let mut funcs = FunctionSection::new();
+    funcs.function(0);
+    module.section(&funcs);
+    let mut code = CodeSection::new();
+    code.function(&Function::new([]));
+    module.section(&code);
+
+    let mut producers = wasm_metadata::Producers::empty();
+    producers.add(
+        "processed-by",
+        env!("CARGO_PKG_NAME"),
+        env!("CARGO_PKG_VERSION"),
+    );
+    let data = wit_component::metadata::encode(resolve, world, encoding, Some(&producers)).unwrap();
+
+    // The custom section name here must start with "component-type" but
+    // otherwise is attempted to be unique here to ensure that this doesn't get
+    // concatenated to other custom sections by LLD by accident since LLD will
+    // concatenate custom sections of the same name.
+    let world_name = &resolve.worlds[world].name;
+    let section_name = format!("component-type:{world_name}");
+
+    // Add our custom section
+    module.section(&CustomSection {
+        name: std::borrow::Cow::Borrowed(&section_name),
+        data: std::borrow::Cow::Borrowed(data.as_slice()),
+    });
+
+    // Append the linking section, so that lld knows the custom section's symbol name
+    let mut linking = LinkingSection::new();
+    let mut symbols = SymbolTable::new();
+    symbols.function(0, 0, Some(&linking_symbol(&world_name)));
+    linking.symbol_table(&symbols);
+    module.section(&linking);
+
+    Ok(module.finish())
+}
