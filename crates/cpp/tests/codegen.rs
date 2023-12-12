49f9411cf13f0045db055da1a127f83d027bb858
Christof Petig <christof.petig@arcor.de>
2023-12-12 20:40:01 +0100

    also test host program

--- crates/cpp/tests/codegen.rs
+++ crates/cpp/tests/codegen.rs
@@ -18,6 +18,16 @@ macro_rules! codegen_test {
                 },
                 verify,
             );
+            test_helpers::run_world_codegen_test(
+                "cpp-host",
+                $test.as_ref(),
+                |resolve, world, files| {
+                    let mut opts = wit_bindgen_cpp::Opts::default();
+                    opts.host = true;
+                    opts.build().generate(resolve, world, files).unwrap()
+                },
+                verify_host,
+            );
         }
     };
 }
@@ -31,21 +41,51 @@ fn verify(dir: &Path, name: &str) {
     );
     let sysroot = sdk_path.join("share/wasi-sysroot");
     let c_src = dir.join(format!("{name}.cpp"));
+    let additional_includes = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").expect("environment variable CARGO_MANIFEST_DIR should get set by cargo")).join("test_headers");
 
     let shared_args = vec![
         "--sysroot",
         sysroot.to_str().unwrap(),
         "-I",
         dir.to_str().unwrap(),
-        "-Wall",
-        "-Wextra",
-        "-Werror",
-        "-Wno-unused-parameter",
+        "-I",
+        additional_includes.to_str().unwrap(),
+        // "-Wall",
+        // "-Wextra",
+        // "-Werror",
+        // "-Wno-unused-parameter",
+        "-std=c++2b",
         "-c",
         "-o",
     ];
 
     let mut cmd = Command::new(sdk_path.join("bin/clang++"));
+    cmd.args(&shared_args);
+    cmd.arg(dir.join("obj.o"));
+    cmd.arg(&c_src);
+    test_helpers::run_command(&mut cmd);
+}
+
+fn verify_host(dir: &Path, name: &str) {
+    let name = name.to_snake_case();
+    let c_src = dir.join(format!("{name}_host.cpp"));
+    let additional_includes = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").expect("environment variable CARGO_MANIFEST_DIR should get set by cargo")).join("test_headers");
+
+    let shared_args = vec![
+        "-I",
+        dir.to_str().unwrap(),
+        "-I",
+        additional_includes.to_str().unwrap(),
+        // "-Wall",
+        // "-Wextra",
+        // "-Werror",
+        // "-Wno-unused-parameter",
+        "-std=c++2b",
+        "-c",
+        "-o",
+    ];
+
+    let mut cmd = Command::new("clang++");
     cmd.args(&shared_args);
     cmd.arg(dir.join("obj.o"));
     cmd.arg(&c_src);
