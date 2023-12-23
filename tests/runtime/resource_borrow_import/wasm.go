c4542ead458f9197c6f67944040eb4d1cd547902
Jiaxiao Zhou (Mossaka) <duibao55328@gmail.com>
2023-12-22 16:36:02 -0800

    feat(go): added the missing [resource-drop] imports for resources
    Signed-off-by: Jiaxiao Zhou (Mossaka) <duibao55328@gmail.com>

--- tests/runtime/resource_borrow_import/wasm.go
+++ tests/runtime/resource_borrow_import/wasm.go
@@ -13,7 +13,9 @@ type Import struct{}
 
 func (e Import) Test(v uint32) uint32 {
 	thing := NewThing(v + 1)
+	defer thing.Drop()
 	return TestResourceBorrowImportTestFoo(thing) + 4
+	
 }
 
 func main() {}
