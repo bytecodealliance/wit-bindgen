52e1b3e5d7ad378d1fc2f457583d988a43d4ec97
Christof Petig <christof.petig@arcor.de>
2024-03-17 12:05:07 +0100

    leak fixed

--- crates/cpp/tests/native_resources/the_world_native.cpp
+++ crates/cpp/tests/native_resources/the_world_native.cpp
@@ -43,7 +43,9 @@ extern "C" void fooX3AfooX2FresourcesX00consume(int32_t arg0) {
   foo::foo::resources::Consume(foo::foo::resources::R::Owned(*objptr));
 }
 extern "C" void fooX3AfooX2FresourcesX00X5Bresource_dropX5Dr(int32_t idx) {
-  foo::foo::resources::R::remove_resource(idx);
+  auto ptr = foo::foo::resources::R::remove_resource(idx);
+  assert(ptr.has_value());
+  foo::foo::resources::R::Dtor(*ptr);
 }
 extern "C" int32_t X5BexportX5DfooX3AfooX2FresourcesX00X5Bresource_newX5Dr(uint8_t* rep) {
   return exports::foo::foo::resources::R::store_resource(std::move(rep));
