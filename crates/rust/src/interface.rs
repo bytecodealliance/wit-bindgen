3236dadcf46af2feab4faf1ce20be5a6d754c3fc
Brian H <brian.hardock@fermyon.com>
2024-05-16 12:13:09 -0600

    fix: make _lift for enum generated code pub
    Signed-off-by: Brian H <brian.hardock@fermyon.com>

--- crates/rust/src/interface.rs
+++ crates/rust/src/interface.rs
@@ -2219,7 +2219,8 @@ impl<'a> {camel}Borrow<'a>{{
             self.src,
             r#"
                 impl {name} {{
-                    pub(crate) unsafe fn _lift(val: {repr}) -> {name} {{
+                    #[doc(hidden)]
+                    pub unsafe fn _lift(val: {repr}) -> {name} {{
                         if !cfg!(debug_assertions) {{
                             return ::core::mem::transmute(val);
                         }}
