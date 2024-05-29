4c57c309558529b906b778a6b14e7846471a9ce5
Christof Petig <christof.petig@arcor.de>
2024-05-30 00:08:05 +0200

    more of the mesh skeleton

---
+++ crates/cpp/tests/native_mesh/mesh/exports-foo-foo-resources.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "exports-foo-foo-resources-R.h"
+#include <cstdint>
+#include <utility>
+// export_interface Interface(Id { idx: 0 })
+namespace exports {
+namespace foo {
+namespace foo {
+namespace resources {
+R Create();
+void Consume(R &&o);
+} // namespace resources
+} // namespace foo
+} // namespace foo
+} // namespace exports
