078c06f5d975fbea1ae79906e9fa5adfdfa8c7d9
Christof Petig <christof.petig@arcor.de>
2024-06-15 23:44:43 +0900

    add wamr example

---
+++ crates/cpp/tests/native_strings/wamr/wasi_dummy.c
@@ -0,0 +1,13 @@
+#include "executor.h"
+
+uint32_t wasi_snapshot_preview1__args_get(void*,uint32_t,uint32_t) {
+    abort();
+}
+
+uint32_t wasi_snapshot_preview1__args_sizes_get(void*,uint32_t,uint32_t) {
+    abort();
+}
+
+void wasi_snapshot_preview1__proc_exit(void*,uint32_t) {
+    abort();
+}
