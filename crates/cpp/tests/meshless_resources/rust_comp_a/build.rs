397b8ed863da969212aebf6596a3b682b10b38cf
Christof Petig <christof.petig@arcor.de>
2024-07-06 14:06:32 +0200

    initial prototype for exported resource in Rust

--- crates/cpp/tests/meshless_resources/rust_comp_a/build.rs
+++ crates/cpp/tests/meshless_resources/rust_comp_a/build.rs
@@ -1,8 +1,7 @@
 use std::env;
 
 fn main() {
-    let source_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    println!("cargo::rustc-link-search=native={}/../component_b", source_dir);
-    println!("cargo::rustc-link-lib=static=component_b");
-    println!("cargo::rustc-link-lib=static=stdc++");
+    let source_dir = env::var("OUT_DIR").unwrap();
+    println!("cargo:rustc-link-search=native={}/deps", source_dir);
+    println!("cargo:rustc-link-lib=dylib=rust_comp_b");
 }
