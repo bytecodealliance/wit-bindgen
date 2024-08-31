b9ede9515082e4c302d4fe9e6e90a6e1858fc967
Christof Petig <christof.petig@arcor.de>
2024-08-31 23:03:45 +0200

    symmetric c++ test

--- crates/cpp/tests/symmetric.rs
+++ crates/cpp/tests/symmetric.rs
@@ -5,6 +5,8 @@ use std::{
     process::Command,
 };
 
+use wit_bindgen_core::wit_parser::{Resolve, WorldId};
+
 fn tests(
     dir_name: &str,
     out_dir: &PathBuf,
@@ -100,14 +102,95 @@ fn tests(
 
     let mut cmd = Command::new("cargo");
     cmd.arg("run")
-        .current_dir(out_dir)
+        .current_dir(&out_dir)
         .env("RUSTFLAGS", "-Ltarget/debug")
         .env("SYMMETRIC_ABI", "1")
         .env("WIT_BINDGEN_DEBUG", "1");
     let status = cmd.status().unwrap();
     assert!(status.success());
 
+    for path in cpp.iter() {
+        let (resolve, world) = resolve_wit_dir(&dir);
+        let world_name = &resolve.worlds[world].name;
+        let cpp_dir = out_dir.join("cpp");
+        drop(fs::remove_dir_all(&cpp_dir));
+        fs::create_dir_all(&cpp_dir).unwrap();
+
+        let snake = world_name.replace("-", "_");
+        let mut files = Default::default();
+        let mut opts = wit_bindgen_cpp::Opts::default();
+        opts.symmetric = true;
+        opts.build().generate(&resolve, world, &mut files).unwrap();
+
+        for (file, contents) in files.iter() {
+            let dst = cpp_dir.join(file);
+            fs::write(dst, contents).unwrap();
+        }
+
+        let compiler = "clang++";
+        let mut cmd = Command::new(compiler);
+        let out_name = cpp_dir.join(format!("lib{}.so", dir_name));
+        cmd.arg(path)
+            .arg(cpp_dir.join(format!("{snake}.cpp")))
+            // .arg(out_dir.join(format!("{snake}_component_type.o")))
+            .arg("-shared")
+            .arg("-fPIC")
+            .arg("-I")
+            .arg(&cpp_dir)
+            .arg("-I")
+            .arg(&(String::from(env!("CARGO_MANIFEST_DIR")) + "/test_headers"))
+            .arg("-Wall")
+            .arg("-Wextra")
+            .arg("-Wno-unused-parameter")
+            .arg("-std=c++17")
+            .arg("-g")
+            .arg("-o")
+            .arg(&out_name);
+        println!("{:?}", cmd);
+        let output = match cmd.output() {
+            Ok(output) => output,
+            Err(e) => panic!("failed to spawn compiler: {}", e),
+        };
+
+        if !output.status.success() {
+            println!("status: {}", output.status);
+            println!("stdout: ------------------------------------------");
+            println!("{}", String::from_utf8_lossy(&output.stdout));
+            println!("stderr: ------------------------------------------");
+            println!("{}", String::from_utf8_lossy(&output.stderr));
+            panic!("failed to compile");
+        } else {
+            let mut tester = out_dir.clone();
+            tester.push("target");
+            tester.push("debug");
+            tester.push("tester");
+            let run = Command::new(tester)
+                .env("LD_LIBRARY_PATH", cpp_dir)
+                .output();
+            match run {
+                Ok(output) => {
+                    if !output.status.success() {
+                        println!("status: {}", output.status);
+                        println!("stdout: ------------------------------------------");
+                        println!("{}", String::from_utf8_lossy(&output.stdout));
+                        println!("stderr: ------------------------------------------");
+                        println!("{}", String::from_utf8_lossy(&output.stderr));
+                        panic!("failed to run");
+                    }
+                }
+                Err(e) => panic!("failed to run tester: {}", e),
+            }
+        }
+    }
+
     Ok(())
+}
+
+fn resolve_wit_dir(dir: &PathBuf) -> (Resolve, WorldId) {
+    let mut resolve = Resolve::new();
+    let (pkg, _files) = resolve.push_path(dir).unwrap();
+    let world = resolve.select_world(pkg, None).unwrap();
+    (resolve, world)
 }
 
 #[test]
