19bca45b7a80ea505c149b53d737e74c54db9935
Christof Petig <christof.petig@arcor.de>
2025-01-04 20:14:48 +0100

    first two stream values arrive in main

--- crates/cpp/tests/symmetric_stream/main/src/main.rs
+++ crates/cpp/tests/symmetric_stream/main/src/main.rs
@@ -2,6 +2,8 @@
 
 // use std::pin::pin;
 
+use std::sync::atomic::Ordering;
+
 use wit_bindgen_symmetric_rt::{async_support::Stream, CallbackState};
 
 #[link(name = "stream")]
@@ -12,27 +14,62 @@ extern "C" {
     ) -> *mut ();
 }
 
-extern "C" fn ready(_arg: *mut ()) -> CallbackState {
-    todo!()
+const DATALEN: usize = 2;
+
+struct CallbackInfo {
+    stream: *mut Stream,
+    data: [u32; DATALEN],
+}
+
+extern "C" fn ready(arg: *mut ()) -> CallbackState {
+    let info = unsafe { &*arg.cast::<CallbackInfo>() };
+    let len = unsafe { &*info.stream }
+        .ready_size
+        .swap(0, Ordering::Acquire);
+    if len > 0 {
+        for i in 0..len as usize {
+            println!("data {}", info.data[i]);
+        }
+        unsafe {
+            ((&*(&*info.stream).vtable).read)(
+                info.stream,
+                info.data.as_ptr().cast_mut().cast(),
+                DATALEN,
+            );
+        };
+        // call again
+        CallbackState::Pending
+    } else {
+        // finished
+        CallbackState::Ready
+    }
 }
 
 fn main() {
     let mut result_stream: *mut () = core::ptr::null_mut();
-    let handle = unsafe {
+    let continuation = unsafe {
         testX3AtestX2Fstream_testX00X5BasyncX5Dcreate(
             core::ptr::null_mut(),
             (&mut result_stream as *mut *mut ()).cast(),
         )
     };
-    assert!(handle.is_null());
+    // function should have completed (not async)
+    assert!(continuation.is_null());
     let handle = result_stream.cast::<Stream>();
-    let mut target = Box::pin([0_u32, 0]);
+    let mut info = Box::pin(CallbackInfo {
+        stream: handle,
+        data: [0, 0],
+    });
     unsafe {
-        ((&*(&*handle).vtable).read)(handle, target.as_mut_ptr().cast(), 2);
+        ((&*(&*handle).vtable).read)(handle, info.data.as_mut_ptr().cast(), DATALEN);
     };
     let read_ready = unsafe { (&*handle).read_ready_event_send };
     let subscription = unsafe { wit_bindgen_symmetric_rt::subscribe_event_send_ptr(read_ready) };
     println!("Register read in main");
-    wit_bindgen_symmetric_rt::register(subscription, ready, ((&mut *target) as *mut u32).cast());
+    wit_bindgen_symmetric_rt::register(
+        subscription,
+        ready,
+        (&*info as *const CallbackInfo).cast_mut().cast(),
+    );
     wit_bindgen_symmetric_rt::run();
 }
