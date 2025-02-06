880d199cc2c6acb92ae6783b295939c8d6045b67
Joel Dice <joel.dice@fermyon.com>
2025-02-06 11:06:32 -0500

    [Rust] fix length calculation in `stream.write` bindings
    When calculating the length we pass to the `stream.write` intrinsic, we need to
    subtract the number of bytes we've already written.  I forgot to do that in my
    original PR.
    
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>

--- crates/rust/src/interface.rs
+++ crates/rust/src/interface.rs
@@ -795,7 +795,7 @@ pub mod vtable{ordinal} {{
                         wit_import,
                         stream,
                         address.add(total * {size}),
-                        u32::try_from(values.len()).unwrap()
+                        u32::try_from(values.len() - (total * {size})).unwrap()
                     ).await
                 }};
 
