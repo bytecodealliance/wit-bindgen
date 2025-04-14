56397cec8dc5fb77e09dc234aad94064e13ffeff
Christof Petig <christof.petig@arcor.de>
2025-04-15 00:49:14 +0200

    stream example skeleton

--- crates/cpp/tests/symmetric_stream/generate.sh
+++ crates/cpp/tests/symmetric_stream/generate.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 (cd stream/src ; ../../../../../../target/debug/wit-bindgen rust ../../wit/async_stream.wit --async none --symmetric)
+(cd stream_cpp; ../../../../../target/debug/wit-bindgen cpp ../wit/async_stream.wit --symmetric)
 cargo fmt
---
+++ crates/cpp/tests/symmetric_stream/stream_cpp/impl.cpp
@@ -0,0 +1,7 @@
+#include "stream_world_cpp.h"
+#include <thread>
+
+wit::stream<uint32_t> exports::test::test::future_test::Create() {
+    // create result
+    // while read, output value, value+1
+}
