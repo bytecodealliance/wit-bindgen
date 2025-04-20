343c6d9291281776931c7998e0d7c8c830d81dcb
Christof Petig <christof.petig@arcor.de>
2025-04-20 21:15:27 +0200

    re-enable normal parameters

--- tests/runtime/fixed-size-list/test.rs
+++ tests/runtime/fixed-size-list/test.rs
@@ -5,12 +5,12 @@ struct Component;
 export!(Component);
 
 impl exports::test::fixed_size_lists::to_test::Guest for Component {
-    // fn list_param(a: [u32; 4]) {
-    //     assert_eq!(a, [1, 2, 3, 4]);
-    // }
-    // fn list_param2(a: [[u32; 2]; 2]) {
-    //     assert_eq!(a, [[1, 2], [3, 4]]);
-    // }
+    fn list_param(a: [u32; 4]) {
+        assert_eq!(a, [1, 2, 3, 4]);
+    }
+    fn list_param2(a: [[u32; 2]; 2]) {
+        assert_eq!(a, [[1, 2], [3, 4]]);
+    }
     // fn list_param3(_a: [i32; 20]) {}
     fn list_result() -> [u8; 8] {
         [b'0', b'1', b'A', b'B', b'a', b'b', 128, 255]
