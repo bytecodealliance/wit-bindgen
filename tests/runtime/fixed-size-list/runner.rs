1362b22387f6c144bf49c0e4a665c4f4ec55f913
Christof Petig <christof.petig@arcor.de>
2025-04-20 16:15:49 +0200

    document and compare test

--- tests/runtime/fixed-size-list/runner.rs
+++ tests/runtime/fixed-size-list/runner.rs
@@ -3,6 +3,6 @@ include!(env!("BINDINGS"));
 use test::fixed_size_lists::to_test::*;
 
 fn main() {
-    list_param([1,2,3,4]);
-    list_param2([[1,2],[3,4]]);
+    list_param([1, 2, 3, 4]);
+    list_param2([[1, 2], [3, 4]]);
 }
