0d579818db081b96e9a46495c89aaebfb3b09b01
Christof Petig <christof.petig@arcor.de>
2025-04-20 16:15:49 +0200

    document and compare test

--- tests/runtime/fixed-size-list/test.rs
+++ tests/runtime/fixed-size-list/test.rs
@@ -5,7 +5,11 @@ struct Component;
 export!(Component);
 
 impl exports::test::fixed_size_lists::to_test::Guest for Component {
-    fn list_param(_a: [u32; 4]) {}
-    fn list_param2(_a: [[u32; 2]; 2]) {}
+    fn list_param(a: [u32; 4]) {
+        assert_eq!(a, [1, 2, 3, 4]);
+    }
+    fn list_param2(a: [[u32; 2]; 2]) {
+        assert_eq!(a, [[1, 2], [3, 4]]);
+    }
     // fn list_param3(_a: [i32; 20]) {}
 }
