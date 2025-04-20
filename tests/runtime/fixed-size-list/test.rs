506ca03b9cd30f5a4df5919653eeb441eb8c7bb3
Christof Petig <christof.petig@arcor.de>
2025-04-20 21:52:47 +0200

    another testcase passing

--- tests/runtime/fixed-size-list/test.rs
+++ tests/runtime/fixed-size-list/test.rs
@@ -11,7 +11,12 @@ impl exports::test::fixed_size_lists::to_test::Guest for Component {
     fn list_param2(a: [[u32; 2]; 2]) {
         assert_eq!(a, [[1, 2], [3, 4]]);
     }
-    // fn list_param3(_a: [i32; 20]) {}
+    fn list_param3(a: [i32; 20]) {
+        assert_eq!(
+            a,
+            [-1, 2, -3, 4, -5, 6, -7, 8, -9, 10, -11, 12, -13, 14, -15, 16, -17, 18, -19, 20]
+        );
+    }
     fn list_result() -> [u8; 8] {
         [b'0', b'1', b'A', b'B', b'a', b'b', 128, 255]
     }
