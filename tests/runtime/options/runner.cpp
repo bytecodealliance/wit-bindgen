7ff30cba3fb8aae6735bceb60250e7da8279e1a7
Christof Petig <christof.petig@arcor.de>
2025-05-10 14:01:33 +0200

    fix options test

--- tests/runtime/options/runner.cpp
+++ tests/runtime/options/runner.cpp
@@ -2,6 +2,21 @@
 #include <limits.h>
 #include <runner_cpp.h>
 
+template <class T>
+static bool equal(T const& a, T const& b) {
+    return a==b;
+}
+static bool equal(wit::string const& a, std::string const& b) {
+    return a.get_view() == std::string_view(b);
+}
+static bool equal(std::optional<wit::string> const& a, std::optional<std::string> const& b) {
+    if (a.has_value() != b.has_value()) return false;
+    if (a.has_value()) {
+        return equal(a.value(), b.value());
+    }
+    return true;
+}
+
 int main()
 {
     using namespace ::test::options::to_test;
