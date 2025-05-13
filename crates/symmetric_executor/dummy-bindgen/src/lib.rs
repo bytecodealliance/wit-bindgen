33525ad58cca09c7efe89c49cd2636ac186e5998
Christof Petig <christof.petig@arcor.de>
2025-05-14 00:32:52 +0200

    link to either symmetric or canonical runtime

--- crates/symmetric_executor/dummy-bindgen/src/lib.rs
+++ crates/symmetric_executor/dummy-bindgen/src/lib.rs
@@ -1,6 +1,10 @@
 // this crate tries to minimize dependencies for symmetric bindings
 
+#[cfg(feature = "symmetric")]
 pub mod rt {
     pub use dummy_rt::rt::maybe_link_cabi_realloc;
-    pub use wit_bindgen_symmetric_rt::async_support;
+    pub use wit_bindgen_symmetric_rt::{async_support, run, EventGenerator, EventSubscription};
 }
+
+#[cfg(feature = "canonical")]
+pub use original::rt;
