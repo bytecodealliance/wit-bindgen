439feef190ca4f4f06ecbb0815706f8204847658
Christof Petig <christof.petig@arcor.de>
2025-06-22 17:45:49 +0200

    clean the code, make it compile with the newest Rust  guest library

--- tests/runtime-async/async/stream-string/runner.rs
+++ tests/runtime-async/async/stream-string/runner.rs
@@ -3,21 +3,20 @@ include!(env!("BINDINGS"));
 use wit_bindgen::rt::async_support;
 
 use crate::a::b::the_test::f;
-use futures::StreamExt;
 
 fn main() {
     async_support::block_on(async {
         let mut stream = f();
         let result = stream.next().await;
-        assert_eq!(result, Some(vec![String::from("Hello")]));
+        assert_eq!(result, Some(String::from("Hello")));
         let result = stream.next().await;
-        assert_eq!(result, Some(vec![String::from("World!")]));
+        assert_eq!(result, Some(String::from("World!")));
         let result = stream.next().await;
-        assert_eq!(result, Some(vec![String::from("From")]));
+        assert_eq!(result, Some(String::from("From")));
         let result = stream.next().await;
-        assert_eq!(result, Some(vec![String::from("a")]));
+        assert_eq!(result, Some(String::from("a")));
         let result = stream.next().await;
-        assert_eq!(result, Some(vec![String::from("stream.")]));
+        assert_eq!(result, Some(String::from("stream.")));
         let result = stream.next().await;
         assert_eq!(result, None);
     });
