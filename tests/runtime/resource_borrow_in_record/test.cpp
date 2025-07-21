454d6885b5b54adb3601a9b80ca9ff64f6ed7b8d
Christof Petig <33882057+cpetig@users.noreply.github.com>
2025-07-21 21:54:06 +0200

    Test the C++ generator in CI (2nd version: with more fixes included and more upstream changes) (#1340)
    * Test the C++ generator in CI
    
    Updating CI configuration to ensure that `cpp` is listed under the
    languages tested.
    
    * Fix all runtime and most codegen tests (#3)
    
    * fix runtime tests
    
    * fix the majority of codegen tests
    
    * fix and simplify the newly added variant-with-data test
    
    * this looks ugly but seems right, it for sure fixes the test
    
    * recognize expected failures
    
    * cargo fmt (missing comma)
    
    * mark the wasi tests as expected failures for now
    
    ---------
    
    Co-authored-by: Alex Crichton <alex@alexcrichton.com>

--- tests/runtime/resource_borrow_in_record/test.cpp
+++ tests/runtime/resource_borrow_in_record/test.cpp
@@ -5,8 +5,8 @@ namespace test_exports = ::exports::test::resource_borrow_in_record::to_test;
 wit::vector<test_exports::Thing::Owned> test_exports::Test(wit::vector<test_exports::Foo> list) {
     auto result = wit::vector<test_exports::Thing::Owned>::allocate(list.size()); 
     for (size_t i = 0; i < list.size(); ++i) {
-        auto str = wit::string::from_view(std::string_view(list[i].thing.get().Get().to_string() + " test"));
-        result.initialize(i, test_exports::Thing::New(std::move(str)));
+        auto str = wit::string::from_view(std::string_view(list[i].thing.get().get_internal() + " test"));
+        result.initialize(i, test_exports::Thing::new_internal(std::move(str)));
     }
     return result;
 }
