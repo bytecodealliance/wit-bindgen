f41ad1fe8a511efdd2dabeec04ba1c5a0ad8e05e
azdle <azdle@azdle.net>
2025-11-26 12:23:12 -0600

    Support custom Rust types for `list`
    Any type that implements `From<Vec<T>>`, `Into<Vec<T>>` can be used for
    a WIT type `list<T>` via the `with` option.

--- crates/rust/tests/codegen.rs
+++ crates/rust/tests/codegen.rs
@@ -31,3 +31,43 @@ mod inline_and_path {
         generate_all,
     });
 }
+
+#[allow(unused)]
+mod newtyped_list {
+    wit_bindgen::generate!({
+        inline: r#"
+        package test:newtyped-list;
+
+        interface ntl {
+            type newtyped-list = list<u8>;
+            type typed-list = list<u8>;
+
+            use-newtyped-list: func(nl: newtyped-list) -> newtyped-list;
+            use-typed-list: func(tl: typed-list) -> typed-list;
+            use-list: func(l: list<u8>) -> list<u8>;
+        }
+
+        world test {
+            import ntl;
+            export ntl;
+        }
+        "#,
+        with: {
+            "test:newtyped-list/ntl/newtyped-list": crate::newtyped_list::NewtypedList,
+        }
+    });
+
+    struct NewtypedList(Vec<u8>);
+
+    impl From<Vec<u8>> for NewtypedList {
+        fn from(value: Vec<u8>) -> Self {
+            NewtypedList(value)
+        }
+    }
+
+    impl From<NewtypedList> for Vec<u8> {
+        fn from(value: NewtypedList) -> Self {
+            value.0
+        }
+    }
+}
