54b5a6de219bf3605c9341c7ef545a3a6ccf6b5d
yowl <scott.waye@hubse.com>
2025-11-27 18:21:11 -0500

    disable the futures test for c#

--- crates/test/src/csharp.rs
+++ crates/test/src/csharp.rs
@@ -45,6 +45,7 @@ impl LanguageMethods for Csharp {
             name,
             "resources-with-streams.wit"
                 | "resources-with-futures.wit"
+                | "futures.wit"
                 | "streams.wit"
                 | "error-context.wit"
                 | "resource-fallible-constructor.wit"
