44225caaac9d2d6b1763f715c5447bc0143964cc
Andrew Steurer <94206073+asteurer@users.noreply.github.com>
2026-01-08 17:17:19 -0600

    feat(go): preventing concurrent reads/writes on streams and futures (#1490)
    fix: adding panic for empty dst slice, removing handle reassignment for futures
    
    
    
    docs(go): adding package comments
    
    
    
    feat(go): add future read concurrency test
    
    
    
    feat(go): adding remaining concurrent read/write tests
    
    
    
    doc(go): adding comments on drop methods
    
    
    
    Update crates/go/src/package/wit_types/wit_stream.go
    
    
    
    Update crates/go/src/package/wit_types/wit_future.go
    
    
    
    Update crates/go/src/package/wit_types/wit_future.go
    
    Co-authored-by: Joel Dice <joel.dice@fermyon.com>

--- tests/runtime-async/async/simple-stream/runner.go
+++ tests/runtime-async/async/simple-stream/runner.go
@@ -11,26 +11,92 @@ func Run() {
 	write := make(chan wit_types.Unit)
 	read := make(chan wit_types.Unit)
 
-	tx, rx := test.MakeStreamUnit()
-	go func() {
-		assertEqual(tx.Write([]wit_types.Unit{wit_types.Unit{}}), 1)
-		assert(!tx.ReaderDropped())
+	{
+		tx, rx := test.MakeStreamUnit()
+		go func() {
+			assertEqual(tx.Write([]wit_types.Unit{wit_types.Unit{}}), 1)
+			assert(!tx.ReaderDropped())
 
-		assertEqual(tx.Write([]wit_types.Unit{wit_types.Unit{}, wit_types.Unit{}}), 2)
+			assertEqual(tx.Write([]wit_types.Unit{wit_types.Unit{}, wit_types.Unit{}}), 2)
 
-		assertEqual(tx.Write([]wit_types.Unit{wit_types.Unit{}, wit_types.Unit{}}), 0)
-		assert(tx.ReaderDropped())
+			assertEqual(tx.Write([]wit_types.Unit{wit_types.Unit{}, wit_types.Unit{}}), 0)
+			assert(tx.ReaderDropped())
 
-		write <- wit_types.Unit{}
-	}()
+			write <- wit_types.Unit{}
+		}()
 
-	go func() {
-		test.ReadStream(rx)
-		read <- wit_types.Unit{}
-	}()
+		go func() {
+			test.ReadStream(rx)
+			read <- wit_types.Unit{}
+		}()
 
-	(<-read)
-	(<-write)
+		(<-read)
+		(<-write)
+	}
+
+	{
+		tx, rx := test.MakeStreamUnit()
+		syncBarrier := make(chan struct{})
+		panicCh := make(chan any, 2)
+
+		for range 2 {
+			go func() {
+				// Because the channel is empty, it will block until it's closed, at which
+				// point all Goroutines will attempt to simultaneously read from the stream.
+				<-syncBarrier
+				panicCh <- checkPanicValue(func() {
+					result := make([]wit_types.Unit, 1)
+					rx.Read(result)
+				})
+			}()
+		}
+		close(syncBarrier)
+
+		go func() {
+			// If this is omitted, the host will see that the "rx.Read" operations aren't paired with 
+			// a "tx.WriteAll" and will result in a "wasm trap: deadlock detected" error. Additionally, 
+			// this is placed after "close(syncBarrier)" to ensure that the panics are resulting from 
+			// concurrent reads, and not from other scenarios that result in a nil handle.
+			tx.WriteAll([]wit_types.Unit{wit_types.Unit{}})
+		}()
+
+		p1, p2 := <-panicCh, <-panicCh
+
+		// One should succeed (nil), one should panic
+		assert((p1 == nil && p2 == "nil handle") || (p1 == "nil handle" && p2 == nil))
+	}
+
+	{
+		tx, rx := test.MakeStreamUnit()
+		syncBarrier := make(chan struct{})
+		panicCh := make(chan any, 2)
+
+		for range 2 {
+			go func() {
+				// Because the channel is empty, it will block until it's closed, at which
+				// point all Goroutines will attempt to simultaneously write to the stream.
+				<-syncBarrier
+				panicCh <- checkPanicValue(func() {
+					tx.WriteAll([]wit_types.Unit{wit_types.Unit{}})
+				})
+			}()
+		}
+		close(syncBarrier)
+
+		go func() {
+			// If this is omitted, the host will see that the "tx.WriteAll" operations aren't paired with 
+			// an "rx.Read" and will result in a "wasm trap: deadlock detected" error. Additionally, 
+			// this is placed after "close(syncBarrier)" to ensure that the panics are resulting from 
+			// concurrent writes, and not from other scenarios that result in a nil handle.
+			result := make([]wit_types.Unit, 1)
+			rx.Read(result)
+		}()
+
+		p1, p2 := <-panicCh, <-panicCh
+
+		// One should succeed (nil), one should panic
+		assert((p1 == nil && p2 == "nil handle") || (p1 == "nil handle" && p2 == nil))
+	}
 }
 
 func assertEqual[T comparable](a, b T) {
@@ -43,4 +109,12 @@ func assert(v bool) {
 	if !v {
 		panic("assertion failed")
 	}
+}
+
+func checkPanicValue(f func()) (value any) {
+	defer func() {
+		value = recover()
+	}()
+	f()
+	return nil
 }
