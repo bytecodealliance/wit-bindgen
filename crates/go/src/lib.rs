2c1b5799501491a5b5fbd084e7eec272a26eaa93
Andrew Steurer <94206073+asteurer@users.noreply.github.com>
2026-03-09 09:33:06 -0500

    feat: add empty.s to generated directories (#1560)
    Signed-off-by: Andrew Steurer <94206073+asteurer@users.noreply.github.com>

--- crates/go/src/lib.rs
+++ crates/go/src/lib.rs
@@ -956,6 +956,14 @@ import (
                         .as_bytes(),
                     ),
                 );
+
+                files.push(
+                    &format!("{prefix}{name}/empty.s"),
+                    r#"// This file exists for testing this package without WebAssembly,
+// allowing empty function bodies with a //go:wasmimport directive.
+// See https://pkg.go.dev/cmd/compile for more information."#
+                        .as_bytes(),
+                );
             }
         }
 
