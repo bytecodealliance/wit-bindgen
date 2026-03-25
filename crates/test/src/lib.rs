5234a97ceb60c2b7e80b5745b98ecb8c32f4d95b
Demetrius Kanios <demetrius@kanios.net>
2026-03-25 00:53:31 -0700

    Begin support for `cargo run test`
    [skip ci]

--- crates/test/src/lib.rs
+++ crates/test/src/lib.rs
@@ -17,6 +17,7 @@ mod config;
 mod cpp;
 mod csharp;
 mod custom;
+mod d;
 mod go;
 mod moonbit;
 mod runner;
@@ -229,6 +230,7 @@ enum Language {
     Csharp,
     MoonBit,
     Go,
+    D,
     Custom(custom::Language),
 }
 
@@ -1322,6 +1324,7 @@ impl Language {
         Language::Csharp,
         Language::MoonBit,
         Language::Go,
+        Language::D,
     ];
 
     fn obj(&self) -> &dyn LanguageMethods {
@@ -1333,6 +1336,7 @@ impl Language {
             Language::Csharp => &csharp::Csharp,
             Language::MoonBit => &moonbit::MoonBit,
             Language::Go => &go::Go,
+            Language::D => &d::D,
             Language::Custom(custom) => custom,
         }
     }
