de65569ba5990136c2ddff4868549a950ff63039
Adam Bratschi-Kaye <a.bratschi-kaye@f5.com>
2026-09-15 20:22:04 +0000

    `guest-rust`: Implement a `Task` handle for spawned futures (#1710)
    * `guest-rust`: Implement a `Task` handle for spawned futures
    
    The `Task` can be awaited to get the returned value of the future, or it
    can be dropped or explicitly canceled to cancel the future. It can also
    be detached to allow running in the background and match the previous
    behavior of `spawn_local`.
    
    It's possible that the component-model task into which the future was
    spawned could be canceled while the `Task` referencing it is still
    alive. In that case, awaiting or canceling the task will return `None`.
    
    * use channel for cancelation
    
    * rename to JoinHandle
    
    * switch test uses
    
    * remove must_use
    
    * check future before cancel
    
    * back to AbortHandle

--- crates/guest-rust/src/rt/async_support/spawn.rs
+++ crates/guest-rust/src/rt/async_support/spawn.rs
@@ -7,7 +7,10 @@ use crate::rt::async_support::BoxFuture;
 use alloc::boxed::Box;
 use alloc::vec::Vec;
 use core::future::Future;
+use core::pin::Pin;
 use core::task::{Context, Poll};
+use futures::channel::oneshot;
+use futures::future::{AbortHandle, Abortable, Aborted};
 use futures::stream::{FuturesUnordered, StreamExt};
 
 /// Any newly-deferred work queued by calls to the `spawn` function while
@@ -94,10 +97,10 @@ impl<'a> Tasks<'a> {
 ///   computations executing within a [`block_on`] call, however, the spawned
 ///   tasks will be executed within that scope. This notably means that for
 ///   [`block_on`] spawned tasks will prevent the [`block_on`] function from
-///   returning, even if a value is available to return.
-///
-/// * There is no handle returned to the spawned task meaning that it cannot be
-///   cancelled or monitored.
+///   returning, even if a value is available to return. If `spawn_local` is
+///   called within a component-model async task which is then terminated (e.g.
+///   by the host) before the future resolves, awating the `JoinHandle` will return
+///   `None`.
 ///
 /// * The task spawned here is executed *concurrently*, not in *parallel*. This
 ///   means that while one future is being polled no other future can be polled
@@ -108,8 +111,52 @@ impl<'a> Tasks<'a> {
 /// exported async function has produced a value this can be used to continue to
 /// execute some more code before the component model async task exits.
 ///
+/// # Cancellation
+///
+/// Dropping the resulting [`JoinHandle`] will detach the task and allow it to
+/// continue running in the background. It can also be explicitly cancelled with
+/// [`JoinHandle::cancel`].
+///
 /// [`block_on`]: crate::block_on
 /// [#1305]: https://github.com/bytecodealliance/wit-bindgen/issues/1305
-pub fn spawn_local(future: impl Future<Output = ()> + 'static) {
-    unsafe { SPAWNED.push(Box::pin(future)) }
+pub fn spawn_local<T: 'static>(future: impl Future<Output = T> + 'static) -> JoinHandle<T> {
+    let (sender, receiver) = oneshot::channel();
+    let (abort, registration) = AbortHandle::new_pair();
+    unsafe {
+        SPAWNED.push(Box::pin(async move {
+            let _ = sender.send(Abortable::new(future, registration).await);
+        }));
+    }
+    JoinHandle { receiver, abort }
+}
+
+/// A handle to a spawned task which can be awaited for its result.
+///
+/// Dropping the handle allows the task to continue running in the background.
+/// Awaiting the handle returns `None` if the task was cancelled or otherwise
+/// terminated without producing a result.
+pub struct JoinHandle<T> {
+    receiver: oneshot::Receiver<Result<T, Aborted>>,
+    abort: AbortHandle,
+}
+
+impl<T> JoinHandle<T> {
+    /// Cancels the spawned task. It is possible the task resolved and produced
+    /// a result before cancelation completed and the [`JoinHandle`] can still
+    /// be awaited to check for that case.
+    pub fn cancel(&mut self) {
+        self.abort.abort();
+    }
+}
+
+impl<T> Future for JoinHandle<T> {
+    type Output = Option<T>;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<T>> {
+        match Pin::new(&mut self.receiver).poll(cx) {
+            Poll::Ready(Ok(Ok(result))) => Poll::Ready(Some(result)),
+            Poll::Ready(Ok(Err(_)) | Err(_)) => Poll::Ready(None),
+            Poll::Pending => Poll::Pending,
+        }
+    }
 }
